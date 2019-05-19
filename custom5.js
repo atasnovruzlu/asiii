68ca543774d277ad63ca5afead6e309acefc7440
atasnovruzlu <cs04122016@gmail.com>
2019-05-19 11:12:58 +0400

    dxsds

--- custom5.js
+++ custom5.js
@@ -19,4 +19,85 @@ function getJSONFile() {
         .catch(err => console.log(err))
 }
 
-getJSONFile();
+// getJSONFile();
+
+
+function getExternalApi() {
+    fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+}
+
+
+// getExternalApi();
+
+
+
+
+
+
+class Request {
+    // GET
+    get(url) {  //Get request
+        return new Promise((resolve, reject) => {
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+
+        })
+
+
+    }
+
+
+    // POST
+
+    post(url, data) {
+
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+                .then(response => response.json())
+                .then(data =>resolve(data))
+                .catch(err => reject(err))
+        })
+
+
+
+    }
+}
+const request = new Request();
+let albums;
+request.get("https://jsonplaceholder.typicode.com/todos")
+    .then(data => {
+        albums = data;
+        console.log(albums);
+
+    })
+    .catch(err => console.log(err))
+
+
+
+
+request.post("https://jsonplaceholder.typicode.com/todos", {
+    Userid: 11,
+    id : 10,
+    name  : "Atash"
+}).then(newAlbum => console.log(newAlbum))
+.catch(err => console.log(err)
+)
+
+// console.log(albums)
+
+
+
+//  PROMISE GET
+
